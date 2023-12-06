6bebfdca646ee673d0de91c6e3a3247ebb9061ca
Kateryna Vasiuk <catherine.vasyuk11@gmail.com>
2023-12-07 01:35:19 +0200

    fix deployment error

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
-
 const app = require("./app");
-
-mongoose.set("strictQuery", true);
 
 const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(8080, () => {
+    app.listen(3000, () => {
       console.log("Database connection successful");
     });
   })
