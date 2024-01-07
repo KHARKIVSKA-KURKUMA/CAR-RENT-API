08702f6a4f5523431fc710f09b399c3c65d7cfcf
Kateryna Vasiuk <catherine.vasyuk11@gmail.com>
2024-01-07 20:42:49 +0200

    add swagger to project

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ global.basedir = __dirname;
 
 const authRouter = require("./routes/api/auth");
 const advertsRouter = require("./routes/api/advert");
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 
@@ -16,8 +18,10 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/users/", authRouter);
+app.use("/users", authRouter);
 app.use("/adverts", advertsRouter);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
